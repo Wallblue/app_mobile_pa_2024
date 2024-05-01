955a7520cf3ab4da5c789639efdac2f1cb52a70f
Wallblue <nrb.busquet@gmail.com>
2024-05-01 14:21:20 +0200

    Add authentication logic and API ineraction

--- app/src/main/java/com/example/autempsdonne/MainActivity.kt
+++ app/src/main/java/com/example/autempsdonne/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.autempsdonne
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +23,41 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Set on Submit listener
+        findViewById<Button>(R.id.auth_submit_btn).setOnClickListener{
+            val identifier = findViewById<EditText>(R.id.auth_username_et).text.toString()
+            val password = findViewById<EditText>(R.id.auth_password_et).text.toString()
+
+            val identifierType = if (identifier.contains('@')) "email" else "username"
+
+            val url = "https://api.autantdonner.fr/api/auth/login"
+            val reqBody = JSONObject().apply {
+                put(identifierType, identifier)
+                put("password", password)
+            }
+
+            val queue = Volley.newRequestQueue(this)
+            val req = JsonObjectRequest(
+                Request.Method.POST, url, reqBody,
+                {content ->
+                    println(content)
+                    if(!content.getBoolean("success"))
+                        println("Wrong Password or Username")
+                    else
+                        getSharedPreferences("autempsdonne", MODE_PRIVATE).edit().putString("token", content.getString("jwt")).apply()
+
+                    // TODO Send to home page
+                },
+                {error ->
+                    if(error.message != null)
+                        Toast.makeText(applicationContext, error.message, Toast.LENGTH_LONG).show()
+                    else
+                        Toast.makeText(applicationContext, R.string.LoginErr, Toast.LENGTH_LONG).show()
+                }
+            )
+            queue.add(req)
         }
     }
 }
