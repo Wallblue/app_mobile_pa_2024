c326f3a4bf6c250342585ac2a78c77ab8d267785
Wallblue <nrb.busquet@gmail.com>
2024-05-01 17:21:35 +0200

    Add users classes

--- app/src/main/java/com/example/autempsdonne/User.kt
+++ app/src/main/java/com/example/autempsdonne/User.kt
@@ -1,4 +1,12 @@
 package com.example.autempsdonne
 
-class User {
-}
+class User(
+    val id: Long,
+    val name: String,
+    val firstName: String,
+    val username: String,
+    val email: String,
+    val phone: String,
+    val authLevel: String,
+    val url: String
+)
--- app/src/main/java/com/example/autempsdonne/UserAdapter.kt
+++ app/src/main/java/com/example/autempsdonne/UserAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.autempsdonne
 
-class UserAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class UserAdapter(val context: Context, var lusers: MutableList<User>) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return this.lusers.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return this.lusers[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return this.lusers[position].id
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val v = convertView ?: LayoutInflater.from(this.context).inflate(R.layout.row_user, null)
+
+        val currentUser = getItem(position) as User
+
+        v.findViewById<TextView>(R.id.tv_user_username).text = currentUser.username
+        v.findViewById<TextView>(R.id.tv_user_email).text = currentUser.email
+
+        return v
+    }
 }
