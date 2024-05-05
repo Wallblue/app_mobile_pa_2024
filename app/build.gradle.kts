c78e2685204cb6eee0a88355e987e8559d7da328
Wallblue <nrb.busquet@gmail.com>
2024-05-06 00:21:18 +0200

    Add Glide library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
 }
