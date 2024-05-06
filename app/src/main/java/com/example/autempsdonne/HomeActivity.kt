1b8efc80b03a0e98f6b3924cb5b90de2a983e832
Wallblue <nrb.busquet@gmail.com>
2024-05-06 11:51:47 +0200

    Rework Home Page with authLevels

--- app/src/main/java/com/example/autempsdonne/HomeActivity.kt
+++ app/src/main/java/com/example/autempsdonne/HomeActivity.kt
@@ -2,13 +2,24 @@ package com.example.autempsdonne
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var scanQrBtn : Button
+    private lateinit var profileBtn : Button
+    private lateinit var readNfcBtn : Button
+    private lateinit var assignNfcBtn : Button
+
+    private var token : String? = null
+    private var authLevel : AuthLevels? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,24 +30,64 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
-        findViewById<Button>(R.id.home_scanQR_btn).setOnClickListener{
-            val i = Intent(this, QRScanActivity::class.java)
-            startActivity(i)
+        this.token = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE).getString("token", "")
+
+        if(this.token == null){
+            Toast.makeText(applicationContext, R.string.NullTokenErr, Toast.LENGTH_LONG).show()
+            finish()
         }
 
-        findViewById<Button>(R.id.home_profile_btn).setOnClickListener{
+        this.authLevel = Utils.getAuthLevel(this.token!!)
+
+        getButtons()
+        setButtonsListeners()
+    }
+
+    private fun setButtonsListeners() {
+        setProfileListener()
+        setReadNfcListener()
+        setScanQrListener()
+        setAssignNfcListener()
+    }
+
+    private fun setProfileListener(){
+        profileBtn.setOnClickListener{
             val i = Intent(this, UserProfileActivity::class.java)
             startActivity(i)
         }
+    }
 
-        findViewById<Button>(R.id.home_read_nfc_btn).setOnClickListener{
+    private fun setReadNfcListener(){
+        readNfcBtn.setOnClickListener{
             val i = Intent(this, ReadNfcActivity::class.java)
             startActivity(i)
         }
+    }
 
-        findViewById<Button>(R.id.home_assignNFC_btn).setOnClickListener{
-            val i = Intent(this, VolunteersListActivity::class.java)
-            startActivity(i)
+    private fun setScanQrListener(){
+        if (this.authLevel == AuthLevels.VOLUNTEER || this.authLevel == AuthLevels.ADMIN) {
+            scanQrBtn.visibility = View.VISIBLE
+            scanQrBtn.setOnClickListener {
+                val i = Intent(this, QRScanActivity::class.java)
+                startActivity(i)
+            }
         }
+    }
+
+    private fun setAssignNfcListener(){
+        if (this.authLevel == AuthLevels.ADMIN) {
+            assignNfcBtn.visibility = View.VISIBLE
+            assignNfcBtn.setOnClickListener {
+                val i = Intent(this, VolunteersListActivity::class.java)
+                startActivity(i)
+            }
+        }
+    }
+
+    private fun getButtons() {
+        this.profileBtn = findViewById(R.id.home_profile_btn)
+        this.readNfcBtn = findViewById(R.id.home_read_nfc_btn)
+        this.scanQrBtn = findViewById(R.id.home_scanQR_btn)
+        this.assignNfcBtn = findViewById(R.id.home_assignNFC_btn)
     }
 }
